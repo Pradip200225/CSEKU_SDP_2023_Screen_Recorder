4718c811d5b3aa1a93890fdcc3ae7e4d7a641799
Pradip200225 <pradip2025@cseku.ac.bd>
2023-04-13 01:44:24 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ScreenRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tmrRecord = new System.Windows.Forms.Timer(this.components);
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(161, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Record";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(333, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Cyan;
+            this.button3.Location = new System.Drawing.Point(523, 344);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 69);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Select Folder";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tmrRecord
+            // 
+            this.tmrRecord.Interval = 15;
+            this.tmrRecord.Tick += new System.EventHandler(this.tmrRecord_Tick);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(219, 34);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(90, 25);
+            this.lblTimer.TabIndex = 3;
+            this.lblTimer.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ScreenRecorder.Properties.Resources.unnamed;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ScreenRecorder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer tmrRecord;
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenRecorder
+{
+    public partial class Form1 : Form
+    {
+        bool folderSelected = false;
+        string outputPath = "";
+        string finalVidName = "FinalVideo.mp4";
+        // Screen recorder object:
+        ScreenRecorder screenRec = new ScreenRecorder(new Rectangle(), "");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Create output path:
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.Description = "Select an Output Folder";
+
+
+            if (folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                outputPath = @folderBrowser.SelectedPath;
+                folderSelected = true;
+
+                //Finish screen recorder object:
+                Rectangle bounds = Screen.FromControl(this).Bounds;
+                screenRec = new ScreenRecorder(bounds, outputPath);
+            }
+            else
+            {
+                MessageBox.Show("Please select an output folder.", "Error");
+            }
+        }
+
+        private void tmrRecord_Tick(object sender, EventArgs e)
+        {
+            screenRec.RecordVideo();
+            screenRec.RecordAudio();
+            lblTimer.Text = screenRec.getElapsed();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderSelected)
+            {
+                screenRec.setVideoName(finalVidName);
+                tmrRecord.Start();
+            }
+
+            else
+            {
+                MessageBox.Show("You must select a output folder before recording", "Error");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tmrRecord.Stop();
+            screenRec.Stop();
+            Application.Restart();
+        }
+    }
+}
